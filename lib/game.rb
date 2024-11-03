5a774f2e31f191ed1052cb6257c1dc596bb5d63d
Zachary Arledge <174385639+vlamm0@users.noreply.github.com>
2024-11-03 09:16:02 -0500

    Play hangman

--- lib/game.rb
+++ lib/game.rb
@@ -2,41 +2,80 @@
 
 # class
 class Game
-  attr_accessor :word, :spaces, :lives
+  attr_accessor :word, :spaces, :lives, :wrong
 
   HOOK = "_\n\s|\n\s|"
   MAN = ["\sO", "\sO\n\s|", "\sO\n\\|", "\sO\n\\|/", "\sO\n\\|/\n\s|", "\sO\n\\|/\n\s|\n/",
          "\sO\n\\|/\n\s|\n/\s\\"].freeze
 
   def initialize
-    self.word = make_wordbank('google-10000-english-no-swears.txt', 5, 12).sample.chomp
+    self.word = make_word('google-10000-english-no-swears.txt', 5, 12)
     self.spaces = Array.new(word.length, '_')
-    self.lives = -1
+    self.lives = 7
+    self.wrong = []
   end
 
-  def make_wordbank(file, min, max)
+  # selects a valid word from the list of words
+  def make_word(file, min, max)
     File.exist?(file) ? words = File.readlines(file) : raise('Terminating program: ***MISSING FILE***')
-    words.select { |word| (word.chomp.length >= min) && (word.chomp.length <= max) }
+    words.select { |word| (word.chomp.length >= min) && (word.chomp.length <= max) }.sample.chomp
   end
 
-  def blanks
-    spaces.reduce('') do |word, space|
-      word += "#{space} "
+  # enumerates the letters/blanks of the guesses
+  def enum(letters)
+    letters.reduce('') do |word, letter|
+      word += "#{letter} "
       word
     end
   end
 
+  # creates hangman picture
   def hangman
-    lives == -1 ? HOOK : "#{HOOK}\n#{MAN[lives].colorize(:red)}"
+    lives == 7 ? 'BEGIN' : "#{HOOK}\n#{MAN[-1 * lives - 1].colorize(:red)}"
   end
 
+  # displays game to user
   def display
-    puts "#{blanks}\n\n#{hangman}\n\nLIFE: #{6 - lives}"
+    puts "#{enum(spaces)}\n\n#{enum(wrong).colorize(:red)}\n\n#{hangman}\n\nLIFE: #{lives}"
   end
 
+  # ensures guesses are letters
   def guess
-    puts "\n\nGUESS A LETTER"
+    print "\n\nGUESS A LETTER:\s"
     letter = gets.chomp
-    letter.match?(/[a-zA-Z]/) && letter.length == 1 ? letter.downcase : guess
+    letter.match?(/[a-zA-Z]/) && letter.length == 1 && !wrong.include?(letter) ? letter.downcase : guess
+  end
+
+  # checks letter and adds guess to appropriate word bank
+  def check_letter(letter)
+    if word.include?(letter)
+      word.split('').each_with_index { |char, index| spaces[index] = char if letter == char }
+    else
+      self.lives -= 1
+      wrong.push(letter)
+    end
+  end
+
+  # checks for remaining lives or if the word is complete
+  def base_case?
+    if lives < 1
+      puts "***GAME OVER***\n#{word.colorize(:green)}"
+    elsif !spaces.include?('_')
+      puts "***YOU WIN***\n#{word.colorize(:green)}"
+    else
+      return false
+    end
+    true
+  end
+
+  # the games logic for different methods
+  def manage
+    # base
+    return if base_case?
+
+    # recursive case
+    display
+    check_letter(guess)
+    manage
   end
 end
--- main.rb
+++ main.rb
@@ -4,16 +4,6 @@ require 'colorize'
 require 'rubocop'
 require_relative 'lib/game'
 
-# # creates a list of words from file, with character restraints
-# def make_wordbank(file, min, max)
-#   File.exist?(file) ? words = File.readlines(file) : raise("Terminating program: ***MISSING FILE***")
-#   word_list = words.select {|word| (word.chomp.length >= min) && (word.chomp.length <= max)}
-# end
-
-# word_bank = make_wordbank("google-10000-english-no-swears.txt", 5, 12)
-# #puts word_bank
+# MAIN
 game = Game.new
-puts game.word
-game.display
-game.guess
-# hangman = ["\sO", "\sO\n\s|", "\sO\n\\|", "\sO\n\\|/", "\sO\n\\|/\n\s|", "\sO\n\\|/\n\s|\n/", "\sO\n\\|/\n\s|\n/\s\\"]
+game.manage
